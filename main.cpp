3ae5a4180b2a07ef650f4367e9a73b7398b913d4
k2supra <andrijkorolevic@gmail.com>
2023-01-25 16:01:26 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 using namespace std;
-int main() { 
+void cube(int num1);
+int main() {
+	int num1;
+	cout << "Enter 1 number: ";
+	cin >> num1;
+	cube(num1);
+}
+void cube(int num1)
+{
+	if (num1 > 0)
+	{
+		cout << num1 << " > 0";
+	}
+	else if (num1 < 0)
+	{
+		cout << num1 << " < 0";
+	}
+	else
+	{
+		cout << num1 << " = 0";
+	}
 
 }
