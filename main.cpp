5b00ae090308ffdb46f7fd175cfdf02ae2e2a5b9
k2supra <andrijkorolevic@gmail.com>
2023-01-25 15:58:51 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 using namespace std;
-int main() { 
+void cube(int num1, int num2);
+int main() {
+	int num1, num2;
+	cout << "Enter 1 number: ";
+	cin >> num1;
+	cout << "Enter 2 number: ";
+	cin >> num2;
+	cube(num1, num2);
+}
+void cube(int num1, int num2)
+{
+	if (num1 > num2)
+	{
+		cout << num1 << " > " << num2;
+	}
+	else if (num1 < num2)
+	{
+		cout << num1 << " < " << num2;
+	}
+	else
+	{
+		cout << num1 << " = " << num2;
+	}
 
 }
