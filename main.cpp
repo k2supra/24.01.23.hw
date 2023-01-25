fdc9d310be030426605de480997d7969c9b20f9a
k2supra <andrijkorolevic@gmail.com>
2023-01-25 15:55:00 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+void cube(int num, int cub);
 int main() { 
-
+	int num, cub = 0;
+	cout << "Enter a number: ";
+	cin >> num;
+	cube(num, cub);
+}
+void cube(int num, int cub)
+{
+	cub = num * num * num;
+	cout << "Cube = " << cub;
 }
