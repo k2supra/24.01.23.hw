d6b559236f30ee0279e5a8815873386bde866b42
k2supra <andrijkorolevic@gmail.com>
2023-01-25 15:51:01 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 using namespace std;
-int main() { 
 
+void light(int s1, int s2);
+
+int main() {
+	int s1, s2 = 2;
+	cout << "Enter a number: ";
+	cin >> s1;
+	light(s1, s2);
+}
+
+void light(int s1, int s2)
+{
+	for (size_t i = s2; i < s1; i++)
+	{
+		if (s1 % i == 0)
+		{
+			cout << " Wrong " << s1;
+		}
+		else
+		{
+			cout << "Light " << s1;
+		}
+		
+	}
+	cout << endl;
+	
 }
