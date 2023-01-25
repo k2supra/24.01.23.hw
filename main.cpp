5cfcedfbe741d7cc5048ad4ff2c8f06781743a05
k2supra <andrijkorolevic@gmail.com>
2023-01-25 13:04:37 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 using namespace std;
-int main() { 
 
+void cube(int s1, int s2);
+
+int main() {
+	int s1, s2;
+	cout << "Enter 1 size: ";
+	cin >> s1;
+	cout << "Enter 2 size: ";
+	cin >> s2;
+	cube(s1, s2);
+}
+
+void cube(int s1, int s2)
+{
+	for (size_t i = 0; i < s2; i++)
+	{
+		for (size_t j = 0; j < s1; j++)
+		{
+			cout << " *";
+		}
+		cout << endl;
+	}
 }
