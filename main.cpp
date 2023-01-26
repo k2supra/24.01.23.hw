c71d1feef82dda1ad1b1d7a94e40e0b37875453e
k2supra <andrijkorolevic@gmail.com>
2023-01-26 11:14:01 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <ctime>
 using namespace std;
-int main() { 
 
+void reversed(int arr[], const int size);
+int main() { 
+	srand(time(0));
+	const int size = 10;
+	int arr[size]{};
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 100;
+		cout << arr[i] << '\t';
+	}
+	/*reverse(begin(arr), end(arr));*/
+	cout << "\n***********\n";
+	reversed(arr, size)
+	/*std::reverse(std::begin(arr),
+		std::end(arr));
+	std::copy(std::begin(arr), std::end(arr),
+		std::ostream_iterator<int>(std::cout, " "));*/
+	/*for (int& i : arr) {
+		cout << i << ' ';
+	}*/
+}
+void reversed(int arr[], const int size) {
+	std::reverse(std::begin(arr),
+		std::end(arr));
+	std::copy(std::begin(arr), std::end(arr),
+		std::ostream_iterator<int>(std::cout, " "));
 }
